ec00d69e4dc38dedec142fc24ed2820ff083059c
aravind-kumar-n <75974198+aravind-kumar-n@users.noreply.github.com>
2021-02-28 17:16:51 +0100

    test2

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,10 +2,6 @@ package hello;
 
 public class Greeter {
     public String sayHello() {
-<<<<<<< HEAD
-        return "Hello world! Swaranshi my darling :) :)";
-=======
         return "Hello world! Swaranshi my darling :)";
->>>>>>> f1a9ce0fa9639f796265c915e57eaa79321c16ba
     }
 }
