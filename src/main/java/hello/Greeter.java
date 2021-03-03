354c2f28cc483d656a76b5135ece755285f7134d
aravind-kumar-n <75974198+aravind-kumar-n@users.noreply.github.com>
2021-03-03 01:13:39 +0100

    Test1 by aravind

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Greeter {
     public String sayHello() {
-        return "Hello world! Swaranshi my darling :) (simplest CICD)";
+        return "Hello world! Swaranshi my darling :) (simplest CICD) check1";
     }
 }
