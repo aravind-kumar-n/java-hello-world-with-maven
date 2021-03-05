69382d3654b343f1d39d9872b25c25d1447cb232
aravind-kumar-n <75974198+aravind-kumar-n@users.noreply.github.com>
2021-03-05 18:36:00 +0100

    friday check1

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Greeter {
     public String sayHello() {
-        return "Hello world! Swaranshi my darling :) (simplest CICD) check1";
+        return "Hello world! Swaranshi my darling :) (simplest CICD) check1 friday";
     }
 }
